351b3f2f9b15ad06c801a22d20091617a81d9c1b
Senán d'Art <darts@tcd.ie>
2019-03-28 14:19:42 +0000

    Probably done?
    I think this is correct but I don't know for certain.

--- Practical 3/proc.c
+++ Practical 3/proc.c
@@ -51,76 +51,46 @@ scheduler(void)
   for(int i = 0; i < NPROC; i++){
     runCountTable[i] = 0;
   }
-  struct proc* priorityQueue [NPROC];
-  for(int i = 0; i < NPROC; i++){
-    priorityQueue[i] = &ptable.proc[i];
-  }
 
   while(runnableFound){ // DO NOT MODIFY
     // Enable interrupts on this processor.
     // sti();
     runnableFound = 0; // DO NOT MODIFY
   
-    for(int i = 0; i < NPROC && !runnableFound; i++){
-      p = priorityQueue[i];
+    //find program to run
+    int lowestRunnable = 0;
+    for(int i = 0; i < NPROC; i++){
+      p = &ptable.proc[i];
+
       if(p->state != RUNNABLE)
         continue;
-      runCountTable[i]++;
-      runnableFound = 1; // DO NOT MODIFY/DELETE/BYPASS 
+      
+      if(runCountTable[p->pid] < runCountTable[lowestRunnable])
+        lowestRunnable = p->pid;
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      //switchuvm(p);
-      p->state = RUNNING;
-
-
-      swtch(p);
-      // p->state should not be running on return here.
-      //switchkvm();
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
-    // release(&ptable.lock);
-    
-    //sort queue (selection sort)
-    // for(int i = NPROC - 1; i > 0; i--){
-    //   int maxIndex = i;
-    //   for(int j = i - 1; j >= 0; j--){
-    //     if(runCountTable[j] > runCountTable[maxIndex]){
-    //       maxIndex = j;
-    //     }
-    //   }
-
-    //   int tmpNum = runCountTable[i];
-    //   runCountTable[i] = runCountTable[maxIndex];
-    //   runCountTable[maxIndex] = tmpNum;
-
-    //   struct proc *tmpProc = priorityQueue[i];
-    //   priorityQueue[i] = priorityQueue[maxIndex];
-    //   priorityQueue[maxIndex] = tmpProc;
-
-    // }
-
-    for(int i = 0; i < NPROC; i++){
-      int minReached = 0;
-      for(int j = i; j >= 1 && minReached != 1; j--){
-        if(runCountTable[j] < runCountTable[j - 1]){
-          int tmpNum = runCountTable[j - 1];
-          runCountTable[j-1] = runCountTable[j];
-          runCountTable[j] = tmpNum;
-
-          struct proc *tmpProc = priorityQueue[j - 1];
-          priorityQueue[j - 1] = priorityQueue[j];
-          priorityQueue[j] = tmpProc;
-        }else{
-          minReached = 1;
-        }
-      }
     }
 
+    p = &ptable.proc[lowestRunnable];
+    if(p->state != RUNNABLE)
+      continue;
+    runCountTable[lowestRunnable]++;
+    runnableFound = 1; // DO NOT MODIFY/DELETE/BYPASS 
+
+    // Switch to chosen process.  It is the process's job
+    // to release ptable.lock and then reacquire it
+    // before jumping back to us.
+    c->proc = p;
+    //switchuvm(p);
+    p->state = RUNNING;
+
+
+    swtch(p);
+    // p->state should not be running on return here.
+    //switchkvm();
+    // Process is done running for now.
+    // It should have changed its p->state before coming back.
+    c->proc = 0;
+ 
   }
   printf("No RUNNABLE process!\n");
 }
